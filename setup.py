07ef4a9440fcfca0ba83181906664d9f295cafeb
Uppu Pavan <uppux.pavan@intel.com>
2022-12-08 10:35:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="",
-    version="1.0.1",
+    name="edgesoftware",
+    version="1@2#3",
     description="A CLI wrapper for management of Intel® Edge Software Hub packages.",
     long_description=long_description,
     long_description_content_type="text/markdown",
